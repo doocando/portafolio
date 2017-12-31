71d0936af5877b6c4b6ac0e3de66af8ad7436d98
Carlos Sarmiento <carlos.sarmiento@outlook.com>
2017-12-31 06:40:55 -0500

    lambda code to deploy build to customer user

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,17 +4,20 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-
-portafolio_bucket = s3.Bucket('i.emptycup.biz')
-build_bucket = s3.Bucket('i-build.emptycup.biz')
-
-portafolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('IemptyCup-BuildPortafolio.zip', portafolio_zip)
-
-with zipfile.ZipFile(portafolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm) 
-        portafolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-        portafolio_bucket.Object(nm).Acl().put(ACL='public-read')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    
+    portafolio_bucket = s3.Bucket('i.emptycup.biz')
+    build_bucket = s3.Bucket('i-build.emptycup.biz')
+    
+    portafolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('IemptyCup-BuildPortafolio.zip', portafolio_zip)
+    
+    with zipfile.ZipFile(portafolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm) 
+            portafolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+            portafolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print 'Job Done!'
+    return 'Done!'
