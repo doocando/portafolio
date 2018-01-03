3da6d129795429f2d3c0e65cb3ddc0480c06a749
Carlos Sarmiento <carlos.sarmiento@outlook.com>
2018-01-03 05:45:13 -0500

    add topic for deploy notifications to own email

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,19 +5,30 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
     
-    portafolio_bucket = s3.Bucket('i.emptycup.biz')
-    build_bucket = s3.Bucket('i-build.emptycup.biz')
+    sns = boto3.resource('sns')
+    topic= sns.Topic('arn:aws:sns:us-east-1:277172742138:DeployPortFolioTopic')
     
-    portafolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('IemptyCup-BuildPortafolio.zip', portafolio_zip)
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        portafolio_bucket = s3.Bucket('i.emptycup.biz')
+        build_bucket = s3.Bucket('i-build.emptycup.biz')
+        
+        portafolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('IemptyCup-BuildPortafolio.zip', portafolio_zip)
+        
+        with zipfile.ZipFile(portafolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm) 
+                portafolio_bucket.upload_fileobj(obj,nm,
+                    ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                portafolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        
+        topic.publish(Subject='Portofolio Deployed', Message = 'Portfolio deployed susccessfully')
     
-    with zipfile.ZipFile(portafolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm) 
-            portafolio_bucket.upload_fileobj(obj,nm,
-                ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-            portafolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    print 'Job Done!'
+        print 'Job Done!'
+    except:
+         topic.publish(Subject='Portofolio Deploy Failed', Message = 'The Portfolio was not deployed susccessfully')
+         raise 
     return 'Done!'
